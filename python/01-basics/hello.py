b407b84e20beccb0509f5a4755dd7ca7c98b5654
karkigaurav675 <karkigaurav675@gmail.com>
2024-06-25 20:53:46 +0530

    added all files

--- python/01-basics/hello.py
+++ python/01-basics/hello.py
@@ -1,3 +1,9 @@
 print ('hello world!')
-print ('this is a test.')
-print ('test change')
+print ("code example")
+def jeff(n):
+    print(n)
+
+jeff(7)
+jeff("this is an example")
+jeff_one = "number one jeff"
+jeff_two = "number two jeff"
---
+++ python/01-basics/newone.py
@@ -0,0 +1,3 @@
+from hello import jeff
+
+jeff("some text")
